0d783fd0601040a96b1bb0e65b0a5639c27816e4
M <gnanasekar.m@capgemini.com>
2018-09-26 11:40:37 +0530

    Maven

---
+++ src/main/java/MAVEN_TRAINING/App.java
@@ -0,0 +1,13 @@
+package MAVEN_TRAINING;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
